90be204063697387f54bed7b3b818f1aec48b6e5
WesternGamer <80211714+WesternGamer@users.noreply.github.com>
2023-01-03 17:13:53 -0500

    Update AssemblyInfo.cs

--- SEVersion/Properties/AssemblyInfo.cs
+++ SEVersion/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("WesternGamer")]
 [assembly: AssemblyProduct("FPSCounter")]
-[assembly: AssemblyCopyright("Copyright © WesternGamer 2022")]
+[assembly: AssemblyCopyright("Copyright © WesternGamer 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
